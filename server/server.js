ceb5a96b829014ace6e3da076795a7932d28dc45
smarajitdasgupta <smarajit.dg@gmail.com>
2020-03-31 17:05:59 +1100

    Update server.js
    changed user passwod for db

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://dbuser1:vTxOcg1iUJyEpsQR@cluster0-tbrfa.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://dbuser1:sdgpakai2K@cluster0-tbrfa.mongodb.net/test?retryWrites=true&w=majority';
+
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
